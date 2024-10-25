d8688f174fc9224cdd139a6444e55df1f3c4c897
PhilipQuirke <philipquirkenz@gmail.com>
2024-10-25 13:49:24 +1300

    create_colormap, pale_color

--- maths_tools/maths_complexity.py
+++ maths_tools/maths_complexity.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import torch
 import transformer_lens.utils as utils
-from quanta_tools import (QType, a_run_attention_intervention, NO_IMPACT_TAG, SubTaskBase, position_name, answer_name, filter_nodes,
+from quanta_tools import (QType, a_run_attention_intervention, NO_IMPACT_TAG, SubTaskBase, position_name, answer_name, filter_nodes, 
     FilterAnd, FilterHead, FilterPosition, FilterAttention, FilterImpact, FilterContains, QCondition, 
-    get_quanta_impact, get_quanta_binary, get_quanta_attention, ALGO_SHADES, ATTN_SHADES, MATH_SUB_SHADES, MATH_ADD_SHADES)
+    get_quanta_impact, get_quanta_binary, get_quanta_attention, get_quanta_fail_perc, create_colormap, pale_color, 
+    ALGO_SHADES, ATTN_SHADES, MATH_SUB_SHADES, MATH_ADD_SHADES, FAIL_SHADES)
 from .maths_utilities import tokens_to_unsigned_int
 from .maths_constants import MathsToken, MathsBehavior, maths_tokens_to_names
 
