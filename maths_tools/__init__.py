eb9b3a32bd65ac9e72f728edafeda963594d66ee
PhilipQuirke <philipquirkenz@gmail.com>
2024-10-26 13:42:12 +1300

    export neg_nd_functions

--- maths_tools/__init__.py
+++ maths_tools/__init__.py
@@ -3,7 +3,7 @@ from .maths_constants import MathsBehavior, MathsToken, MathsTask, maths_tokens_
 from .maths_utilities import set_maths_vocabulary, set_maths_question_meanings, int_to_answer_str, tokens_to_unsigned_int, tokens_to_answer
 from .maths_data_generator import maths_data_generator_addition, maths_data_generator_subtraction, maths_data_generator_multiplication, maths_data_generator, maths_data_generator_mixed, make_maths_questions_and_answers, MixedMathsDataset, get_mixed_maths_dataloader
 from .maths_search_add import add_ss_functions, add_sc_functions, add_sa_functions, add_st_functions
-from .maths_search_sub import sub_mt_functions, sub_gt_functions, sub_mb_functions, sub_md_functions
+from .maths_search_sub import sub_mt_functions, sub_gt_functions, sub_mb_functions, sub_md_functions, neg_nd_functions
 from .maths_search_mix import run_strong_intervention, run_weak_intervention, SubTaskBaseMath, opr_functions, sgn_functions
 from .maths_pca import _build_title_and_error_message, pca_op_tag, plot_pca_for_an, manual_nodes_pca, plot_nodes_pca_start_core, plot_nodes_pca_end
 
