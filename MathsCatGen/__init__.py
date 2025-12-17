d0601663c26a364f46273833ee1bccc4b286c320
Philip Quirke <PhilipQuirke@users.noreply.github.com>
2025-12-17 14:01:55 -0800

    Update __init__.py

--- MathsCatGen/__init__.py
+++ MathsCatGen/__init__.py
@@ -1,3 +1,3 @@
 from MathsCatGen.maths_catgen import (
-    get_tasks, get_prompt_template, is_ground_truth_correct, test_is_ground_truth_correct, 
+    get_maths_tasks, get_prompt_template, is_ground_truth_correct, test_is_ground_truth_correct, 
     generate_number_pairs, calculate_ground_truth, generate_synthetic_data )
